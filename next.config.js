aff951f044c8c80e5cd723fdadea2a40297f9637
Bekir Can Baykal <bcbsoftware@hotmail.com>
2025-02-07 13:23:47 +0100

    Solves the issue of "Cannot find module 'next-intl/plugin'"

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
-const createNextIntlPlugin = require('next-intl/plugin');
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
 }
+
+// Using dynamic import for next-intl plugin
+const withNextIntl = require('next-intl/plugin')();
 
 module.exports = withNextIntl(nextConfig);
