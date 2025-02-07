7013c981741d85e8f263b5d10564be8f8fce83b5
Bekir Can Baykal <bcbsoftware@hotmail.com>
2025-02-07 14:35:32 +0100

    Adds pdfjs-dist required canvas package. Removes obsolete line from pre-commit file.

--- next.config.js
+++ next.config.js
@@ -1,10 +1,40 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
-}
+  webpack: (config) => {
+    // Grab the existing rule that handles SVG imports
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    );
 
-// Using dynamic import for next-intl plugin
-const withNextIntl = require('next-intl/plugin')();
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/, // *.svg?url
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        resourceQuery: { not: /url/ }, // exclude if *.svg?url
+        use: ['@svgr/webpack'],
+      }
+    );
+
+    config.resolve.alias.canvas = false;
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      canvas: false,
+      encoding: false
+    };
+
+    return config;
+  },
+}
 
 module.exports = withNextIntl(nextConfig);
